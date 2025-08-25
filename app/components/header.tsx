14bfca19af7b568dffd024fd51223434d35b45fc
Sia Billal Quba <siabillal@gmail.com>
2025-08-26 00:17:29 +0700

    feat: implement navigation links in header component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router";
+
+const navigationLinks = [
+  { to: "/", text: "Home" },
+  { to: "/products", text: "Products" },
+  { to: "/register", text: "Register" },
+  { to: "/login", text: "Login" },
+  { to: "/dashboard", text: "Dashboard" },
+];
+
 export function Header() {
   return (
     <header className="bg-white py-6">
@@ -13,20 +23,20 @@ export function Header() {
 
         {/* Navigation links centered below logo */}
         <nav className="flex justify-center">
-          <ul className="flex space-x-8 text-emerald-900/90 font-bold">
-            <li>
-              <a href="/" className="hover:text-black transition-colors">
+          <ul className="flex text-emerald-900/90 font-semibold py-2 ">
+            {navigationLinks.map((navigationLink) => (
+              <li key={navigationLink.to}>
+                <Link
+                  to={navigationLink.to}
+                  className="px-3 py-1 rounded-full hover:text-black transition-colors"
+                >
+                  {navigationLink.text}
+                </Link>
+                {/* <a href="/" className="hover:text-black transition-colors">
                 Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="/products"
-                className="hover:text-black transition-colors"
-              >
-                Products
-              </a>
-            </li>
+              </a> */}
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
