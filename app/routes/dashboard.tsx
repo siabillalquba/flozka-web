64ad669fec47f4bf1ba1f2f8c659b5b99d319c6b
Sia Billal Quba <siabillal@gmail.com>
2025-09-03 23:50:49 +0700

    feat: setup cookie session storage

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,7 +1,18 @@
+import { getSession } from "~/sessions";
 import type { Route } from "./+types/dashboard";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Dashboard" }];
+}
+
+export async function clientLoader({ request }: Route.ClientLoaderArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+
+  const token = session.get("token");
+
+  console.log({ token });
+
+  return null;
 }
 
 export default function DashboardRoute({}: Route.ComponentProps) {
