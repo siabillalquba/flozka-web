e8f96ec5b84a2e7010cbe2c15660d5dcd27dd0ee
Sia Billal Quba <siabillal@gmail.com>
2025-09-07 21:01:32 +0700

    feat: dashboard get /auth/me with token

---
+++ app/modules/user/type.ts
@@ -0,0 +1,7 @@
+export type UserAuthMe = {
+  id: string;
+  email: string;
+  fullName: string;
+  createdAt: string;
+  updatedAt: string;
+};
--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,24 +1,43 @@
 import { getSession } from "~/sessions";
 import type { Route } from "./+types/dashboard";
+import type { UserAuthMe } from "~/modules/user/type";
+import { Card } from "~/components/ui/card";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Dashboard" }];
 }
 
-export async function clientLoader({ request }: Route.ClientLoaderArgs) {
+export async function loader({ request }: Route.ClientLoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
 
   const token = session.get("token");
 
-  console.log({ token });
+  const response = await fetch(
+    `${import.meta.env.VITE_BACKEND_API_URL}/auth/me`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
 
-  return null;
+  const user: UserAuthMe = await response.json();
+
+  return { user };
 }
 
-export default function DashboardRoute({}: Route.ComponentProps) {
+export default function DashboardRoute({ loaderData }: Route.ComponentProps) {
+  const { user } = loaderData;
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="flex flex-col items-center">
+      <div className="w-full max-w-xs">
+        <h1>Dashboard</h1>
+        <Card>
+          <h2>{user.fullName}</h2>
+          <p>{user.email}</p>
+        </Card>
+      </div>
     </div>
   );
 }
